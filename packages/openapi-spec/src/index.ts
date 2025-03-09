2f34ec4618741a3219ba9b3315cb7f4b18298ba2
Christian <chrille_0313@hotmail.com>
2025-03-09 04:43:53 +0100

    fix: fix relative import not working in esm

--- packages/openapi-spec/src/index.ts
+++ packages/openapi-spec/src/index.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import YAML from 'yaml';
 import type { paths, components, operations } from './openapi';
+import { fileURLToPath } from 'url';
 
 export interface OpenAPI {
   paths: paths;
@@ -9,8 +10,9 @@ export interface OpenAPI {
   operations: operations;
 }
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, 'openapi.yaml');
 const fileContents = fs.readFileSync(filePath, 'utf8');
-const openapiSpec: OpenAPI = YAML.parse(fileContents);
-
-export default openapiSpec;
+const openApiSpec: OpenAPI = YAML.parse(fileContents);
+export default openApiSpec;
