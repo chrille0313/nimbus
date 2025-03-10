c4b2d1ae58282fc1448b5591c0e12e5fb717adcc
Christian <chrille_0313@hotmail.com>
2025-03-11 00:15:12 +0100

    refactor: change name of api type

--- packages/openapi-spec/src/index.ts
+++ packages/openapi-spec/src/index.ts
@@ -4,7 +4,7 @@ import YAML from 'yaml';
 import type { paths, components, operations } from './openapi';
 import { fileURLToPath } from 'url';
 
-export interface OpenAPI {
+export interface APISpec {
   paths: paths;
   components: components;
   operations: operations;
@@ -14,5 +14,5 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, 'openapi.yaml');
 const fileContents = fs.readFileSync(filePath, 'utf8');
-const openApiSpec: OpenAPI = YAML.parse(fileContents);
+const openApiSpec: APISpec = YAML.parse(fileContents);
 export default openApiSpec;
