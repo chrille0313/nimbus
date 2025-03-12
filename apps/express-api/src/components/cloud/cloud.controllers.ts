24fe0b66d87e12c58f13d9ff7559d64a58a1992d
Christian <chrille_0313@hotmail.com>
2025-03-12 13:59:50 +0100

    refactor: factor out dto creation + add error

--- apps/express-api/src/components/cloud/cloud.controllers.ts
+++ apps/express-api/src/components/cloud/cloud.controllers.ts
@@ -1,7 +1,10 @@
 import * as express from 'express';
+import { NotFoundError } from '@/types/errors';
 import { CloudService } from './cloud.service';
+import { CloudDTOFactory } from './cloud.dto';
 
 const cloudService = new CloudService();
+const dtoFactory = new CloudDTOFactory();
 
 export async function getClouds(req: express.Request, res: express.Response) {
   const authenticatedUser = req.context.user;
@@ -9,21 +12,11 @@ export async function getClouds(req: express.Request, res: express.Response) {
   res.jsend.success(clouds);
 }
 
-export async function createCloud(req: express.Request, res: express.Response) {
-  const requestDTO = {
-    name: req.body.name,
-    allocatedStorage: req.body.allocatedStorage
-  };
-  const authenticatedUser = req.context.user;
-  const createdCloud = await cloudService.createCloud(requestDTO, authenticatedUser.id);
-  res.status(201).jsend.success(createdCloud);
-}
-
 export async function getCloud(req: express.Request, res: express.Response) {
-  const requestDTO = { id: req.params.id };
+  const requestDTO = dtoFactory.get(req);
 
   if (!requestDTO.id) {
-    throw new Error('Not Found');
+    throw new NotFoundError();
   }
 
   const authenticatedUser = req.context.user;
@@ -31,17 +24,24 @@ export async function getCloud(req: express.Request, res: express.Response) {
   const cloud = await cloudService.getCloudById(requestDTO.id, authenticatedUser.id);
 
   if (!cloud) {
-    throw new Error('Not Found'); // FIXME: handle properly
+    throw new NotFoundError();
   }
 
   res.jsend.success(cloud);
 }
 
+export async function createCloud(req: express.Request, res: express.Response) {
+  const requestDTO = dtoFactory.create(req);
+  const authenticatedUser = req.context.user;
+  const createdCloud = await cloudService.createCloud(requestDTO, authenticatedUser.id);
+  res.status(201).jsend.success(createdCloud);
+}
+
 export async function deleteCloud(req: express.Request, res: express.Response) {
-  const requestDTO = { id: req.params.id };
+  const requestDTO = dtoFactory.delete(req);
 
   if (!requestDTO.id) {
-    throw new Error('Not Found'); // FIXME: handle properly
+    throw new NotFoundError();
   }
 
   const authenticatedUser = req.context.user;
@@ -49,7 +49,7 @@ export async function deleteCloud(req: express.Request, res: express.Response) {
   const cloud = await cloudService.getCloudById(requestDTO.id, authenticatedUser.id);
 
   if (!cloud) {
-    throw new Error('Not Found'); // FIXME: handle properly
+    throw new NotFoundError();
   }
 
   await cloudService.deleteCloudById(requestDTO.id, authenticatedUser.id);
---
+++ apps/express-api/src/components/cloud/cloud.dto.ts
@@ -0,0 +1,33 @@
+import { Request } from 'express';
+
+export class CloudDTOFactory {
+  get(req: Request) {
+    return {
+      id: req.params.id,
+      name: req.body.name,
+      allocatedStorage: req.body.allocatedStorage
+    };
+  }
+
+  create(req: Request) {
+    return {
+      name: req.body.name,
+      allocatedStorage: req.body.allocatedStorage
+    };
+  }
+
+  update(req: Request) {
+    return {
+      id: req.params.id,
+      name: req.body.name,
+      image: req.body.image,
+      allocatedStorage: req.body.allocatedStorage
+    };
+  }
+
+  delete(req: Request) {
+    return {
+      id: req.params.id
+    };
+  }
+}
