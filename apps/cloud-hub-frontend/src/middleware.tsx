f2e32e976299669731d664ffa110879af9226661
Christian <chrille_0313@hotmail.com>
2025-03-18 10:17:02 +0100

    perf: optimize middleware invocations

--- apps/cloud-hub-frontend/src/middleware.tsx
+++ apps/cloud-hub-frontend/src/middleware.tsx
@@ -2,14 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getSessionCookie } from 'better-auth/cookies';
 
 const protectedRoutes = ['^/clouds/*', '^/dashboard/*', '^/shared/*', '^/auth/sign-out/*'];
-const protectedRoutesWhenAuthenticated = ['^/auth/sign-in/*', '^/auth/sign-up/*'];
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isProtected = protectedRoutes.some((route) => new RegExp(route).test(path));
-  const isAuthProtected = protectedRoutesWhenAuthenticated.some((route) =>
-    new RegExp(route).test(path)
-  );
 
   const sessionCookie = getSessionCookie(request);
 
@@ -17,13 +13,9 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/auth/sign-in', request.url));
   }
 
-  if (isAuthProtected && sessionCookie) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
-
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [...protectedRoutes, ...protectedRoutesWhenAuthenticated]
+  matcher: ['/clouds', '/dashboard', '/shared', '/auth/sign-out']
 };
