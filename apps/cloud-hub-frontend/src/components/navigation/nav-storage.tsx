a27f92d2c64090880e19dc2b84bb39b34f28d7c8
Christian <chrille_0313@hotmail.com>
2025-03-18 10:19:28 +0100

    feat: refactor to server component

--- apps/cloud-hub-frontend/src/components/navigation/nav-storage.tsx
+++ apps/cloud-hub-frontend/src/components/navigation/nav-storage.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-import { type LucideIcon } from 'lucide-react';
-
 import {
   SidebarContent,
   SidebarGroup,
@@ -11,12 +7,13 @@ import {
   SidebarMenuItem
 } from '@repo/ui/components/sidebar';
 import Link from 'next/link';
+import React from 'react';
 
 export interface NavStorageProps {
   items: {
     title: string;
     url: string;
-    icon?: LucideIcon;
+    icon?: React.ReactNode;
     isActive?: boolean;
     items?: {
       title: string;
@@ -35,7 +32,7 @@ export function NavStorage({ items }: NavStorageProps) {
             <SidebarMenuItem key={item.title}>
               <SidebarMenuButton tooltip={item.title} asChild>
                 <Link href={item.url}>
-                  {item.icon && <item.icon />}
+                  {item.icon && item.icon}
                   <span>{item.title}</span>
                 </Link>
               </SidebarMenuButton>
