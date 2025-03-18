a8393fbba1f7c30a00fc1a5b286d80aa819405e3
Christian <chrille_0313@hotmail.com>
2025-03-18 16:54:36 +0100

    feat: add cloud page (static)

---
+++ apps/cloud-hub-frontend/src/app/(authenticated)/clouds/page.tsx
@@ -0,0 +1,27 @@
+import { CloudCard } from '@/components/cards/cloud-card';
+import { requireSession } from '@/lib/utils';
+import { SidebarProvider } from '@repo/ui/components/sidebar';
+
+export default async function CloudsPage() {
+  await requireSession();
+
+  return (
+    <SidebarProvider>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          <CloudCard />
+          <CloudCard />
+          <CloudCard />
+          <CloudCard />
+          <CloudCard />
+          <CloudCard />
+          <CloudCard />
+          <CloudCard />
+          <CloudCard />
+          <CloudCard />
+        </div>
+        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+      </div>
+    </SidebarProvider>
+  );
+}
---
+++ apps/cloud-hub-frontend/src/components/cards/cloud-card.tsx
@@ -0,0 +1,72 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@repo/ui/components/dropdown-menu';
+import { Button } from '@repo/ui/components/button';
+import { Card, CardAction, CardDescription, CardHeader, CardTitle } from '@repo/ui/components/card';
+import { Download, EllipsisVertical, Pencil, Settings, Share, Trash } from 'lucide-react';
+import { AspectRatio } from '@repo/ui/components/aspect-ratio';
+import PlaceholderImage from '@public/images/placeholder.svg';
+
+export interface CloudCardProps {}
+
+export function CloudCard(props: CloudCardProps) {
+  return (
+    <Card className="pt-0 overflow-hidden">
+      <AspectRatio ratio={2} className="relative">
+        <Image src={PlaceholderImage} alt="Cloud Image" style={{ objectFit: 'cover' }} fill />
+      </AspectRatio>
+      <CardHeader>
+        <CardTitle>Cloud</CardTitle>
+        <CardDescription>8/10 GB</CardDescription>
+        <CardAction>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <EllipsisVertical size={100} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <Pencil />
+                  Rename
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link href="#">
+                    <Trash />
+                    Delete
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <Share />
+                  Share
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Download />
+                  Download
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link href="#">
+                  <Settings />
+                  Settings
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </CardAction>
+      </CardHeader>
+    </Card>
+  );
+}
