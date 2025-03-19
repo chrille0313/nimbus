1c55853d2f64f3b99963174625da093d44c1bb6b
Christian <chrille_0313@hotmail.com>
2025-03-19 22:19:12 +0100

    fix: remove invalid imports

--- apps/cloud-hub-frontend/src/app/(authenticated)/clouds/new/page.tsx
+++ apps/cloud-hub-frontend/src/app/(authenticated)/clouds/new/page.tsx
@@ -1,13 +1,5 @@
 import { CreateCloudForm } from '@/components/forms/create-cloud-form';
-import { EmailField } from '@/components/forms/email-field';
-import { PasswordField } from '@/components/forms/password-field';
-import { Button } from '@repo/ui/components/button';
 import { Card, CardContent } from '@repo/ui/components/card';
-import { Loader2 } from 'lucide-react';
-import Link from 'next/link';
-import { Form } from 'react-hook-form';
-import Image from 'next/image';
-import LoginSideImage from '@public/images/space-sky.jpg';
 
 export default function NewCloudPage() {
   return (
