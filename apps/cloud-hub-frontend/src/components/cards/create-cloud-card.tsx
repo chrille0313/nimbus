d0e989459f58e944b07cefb279b30627e70d8603
Christian <chrille_0313@hotmail.com>
2025-03-20 00:42:53 +0100

    feat: update styling

--- apps/cloud-hub-frontend/src/components/cards/create-cloud-card.tsx
+++ apps/cloud-hub-frontend/src/components/cards/create-cloud-card.tsx
@@ -5,8 +5,12 @@ import Link from 'next/link';
 
 export function CreateCloudCard() {
   return (
-    <Button variant="ghost" className="transition-all ease-in-out duration-250" asChild>
-      <Card className="w-full h-full shadow-none border-dashed border-2 p-0">
+    <Button
+      variant="ghost"
+      className="transition-all ease-in-out duration-250 border-dashed border-2 dark:hover:bg-accent/25"
+      asChild
+    >
+      <Card className="w-full h-full shadow-none p-0">
         <Link href="/clouds/new" className="w-full h-full flex justify-center items-center">
           <Plus className="text-border size-8" />
         </Link>
