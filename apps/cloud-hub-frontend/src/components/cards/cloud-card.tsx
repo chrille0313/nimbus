0dee9f30d69c1769aff87c91688b55ba5eae7cd5
Christian <chrille_0313@hotmail.com>
2025-03-19 07:23:54 +0100

    feat: make cloud card clickable
    Also adjusted styling a tiny bit

--- apps/cloud-hub-frontend/src/components/cards/cloud-card.tsx
+++ apps/cloud-hub-frontend/src/components/cards/cloud-card.tsx
@@ -18,55 +18,57 @@ export interface CloudCardProps {}
 
 export function CloudCard(props: CloudCardProps) {
   return (
-    <Card className="pt-0 overflow-hidden">
-      <AspectRatio ratio={2} className="relative">
-        <Image src={PlaceholderImage} alt="Cloud Image" className="object-cover" fill />
-      </AspectRatio>
-      <CardHeader>
-        <CardTitle>Cloud</CardTitle>
-        <CardDescription>8/10 GB</CardDescription>
-        <CardAction>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <EllipsisVertical size={100} />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <Pencil />
-                  Rename
-                </DropdownMenuItem>
+    <Link href="#">
+      <Card className="pt-0 overflow-hidden hover:shadow-xl transition-all ease-in-out duration-250">
+        <AspectRatio ratio={2} className="relative">
+          <Image src={PlaceholderImage} alt="Cloud Image" className="object-cover" fill />
+        </AspectRatio>
+        <CardHeader>
+          <CardTitle>Cloud</CardTitle>
+          <CardDescription>8/10 GB</CardDescription>
+          <CardAction>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <EllipsisVertical className="size-5" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <Pencil />
+                    Rename
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="#">
+                      <Trash />
+                      Delete
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <Share />
+                    Share
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Download />
+                    Download
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
                 <DropdownMenuItem asChild>
                   <Link href="#">
-                    <Trash />
-                    Delete
+                    <Settings />
+                    Settings
                   </Link>
                 </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <Share />
-                  Share
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Download />
-                  Download
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem asChild>
-                <Link href="#">
-                  <Settings />
-                  Settings
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </CardAction>
-      </CardHeader>
-    </Card>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </CardAction>
+        </CardHeader>
+      </Card>
+    </Link>
   );
 }
