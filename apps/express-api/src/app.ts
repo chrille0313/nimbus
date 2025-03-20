124486383b6f565c643cc5045c1176ca2248bafb
Hampooo <hampe.akerlund@gmail.com>
2025-03-20 02:46:12 +0100

    refactor:  uses controllers import instead

--- apps/express-api/src/app.ts
+++ apps/express-api/src/app.ts
@@ -1,11 +1,12 @@
 import { createServer } from '@/utils/server';
 import { Express } from 'express-serve-static-core';
-import * as api from './controllers';
+import controllers from './controllers';
 import config from './config';
 import OpenAPISpecification from '@repo/openapi-spec';
 
-export const app = createServer(api, OpenAPISpecification)
-  .then((server: Express) => {
+export const app = createServer(controllers, OpenAPISpecification)
+  
+.then((server: Express) => {
     server.listen(config.port, config.hostname);
   })
   .catch((error: Error) => {
